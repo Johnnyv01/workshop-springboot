21cf64777aa83694269413c5d0d4a20cfd4c62f4
Jonathan Vicente <jonathan-priston@hotmail.com>
2023-03-19 12:27:08 -0300

    H2 database, test profile, JPA

--- src/main/java/com/jonathan/course/entities/User.java
+++ src/main/java/com/jonathan/course/entities/User.java
@@ -3,8 +3,19 @@ package com.jonathan.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "`user`")
 public class User implements Serializable{
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
